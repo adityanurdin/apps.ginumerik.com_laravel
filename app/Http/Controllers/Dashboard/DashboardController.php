46fb6fa30e79b885efa0dd90166ecaaa10f6d982
Muhammad Aditya Nurdin <adityanurdin0@gmail.com>
2020-06-26 19:08:33 +0700

    add counting module on dashboard

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Dashboard;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\User;
+
 class DashboardController extends Controller
 {
     /**
@@ -14,7 +16,8 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('admin.dashboard.index');
+        $users = User::get();
+        return view('admin.dashboard.index', compact('users'));
     }
 
     /**
--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -14,6 +14,70 @@ Dashboard
     @if (Auth::user()->role == 'guest')
         Hallo {{ Auth::user()->name }}, silahkan mengubungi admin untuk mendapatkan role agar mendapatkan
         akses ke aplikasi.
+    @else
+    <div class="row">
+      <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <div class="card card-statistic-1">
+          <div class="card-icon bg-primary">
+            <i class="far fa-user"></i>
+          </div>
+          <div class="card-wrap">
+            <div class="card-header">
+              <h4>Total Users</h4>
+            </div>
+            <div class="card-body">
+              {{ $users->count() }}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <div class="card card-statistic-1">
+          <div class="card-icon bg-danger">
+            <i class="fas fa-money-bill-wave-alt"></i>
+          </div>
+          <div class="card-wrap">
+            <div class="card-header">
+              <h4>Income</h4>
+            </div>
+            <div class="card-body">
+              Rp. 400JT
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <div class="card card-statistic-1">
+          <div class="card-icon bg-warning">
+            <i class="far fa-file"></i>
+          </div>
+          <div class="card-wrap">
+            <div class="card-header">
+              <h4>Requests</h4>
+            </div>
+            <div class="card-body">
+              1,201
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <div class="card card-statistic-1">
+          <div class="card-icon bg-success">
+            <i class="fas fa-circle"></i>
+          </div>
+          <div class="card-wrap">
+            <div class="card-header">
+              <h4>Company</h4>
+            </div>
+            <div class="card-body">
+              47
+            </div>
+          </div>
+        </div>
+      </div>                  
+    </div>
+    
     @endif
   </div>
 </section>
