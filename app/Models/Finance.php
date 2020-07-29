6fbc0a13b31b2530b8f73cb921f9d1d2098e5879
Muhammad Aditya Nurdin <adityanurdin0@gmail.com>
2020-07-29 11:11:35 +0700

    add muttator

--- app/Models/Finance.php
+++ app/Models/Finance.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Dit;
 
 class Finance extends Model
 {
     protected $guarded = [];
+    protected $appends = ['total_bayar_ppn'];
+
+    public function getTotalBayarPpnAttribute()
+    {
+        $PPn = $this->attributes['total_bayar'] * 0.1;
+        return $this->attributes['total_bayar'] + $PPn;
+    }
 }
