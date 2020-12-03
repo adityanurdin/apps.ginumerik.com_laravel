4fd03ef7ba59072fa532d5984039f5ac90b33ad8
Aditya Nurdin <adityanurdin0@gmail.com>
2020-12-04 06:49:09 +0700

    change color & add logo @ login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,8 +9,13 @@
     <div class="container mt-5">
       <div class="row">
         <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
-          <div class="login-brand" style="font-weight: bold;">
+          {{-- <div class="login-brand" style="font-weight: bold;">
               GINUMERIK
+            </div> --}}
+            <div class="login-brand" style="font-weight: bold;">
+                <img src="{{asset('assets/img/logo2.jpg')}}" draggable="false" alt="logo" width="100" class="shadow-light rounded-circle">
+                <br>
+                GINUMERIK
             </div>
             @if(session()->has('info'))
             <div class="alert alert-primary">
