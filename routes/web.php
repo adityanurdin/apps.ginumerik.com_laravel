652003af6a1e542da0650f5b71246cfe6f08a3ff
adityanurdin <adityanurdin0@gmail.com>
2023-02-18 16:55:42 +0700

    fix summary teknis routing

--- routes/web.php
+++ routes/web.php
@@ -189,7 +189,7 @@ Route::group(['middleware' => 'SETUP'], function() {
             Route::resource('finance', 'Dashboard\FinanceController')->except(['edit']);
         });
 
-        Route::get('teknis/summary', 'Dashboard\TeknisController@summary')->name('teknis.summary')->middleware('Admin');
+        Route::get('teknis/summary', 'Dashboard\TeknisController@summary')->name('teknis.summary');
         // Route::get('teknis/summary', 'Dashboard\TeknisController@summary')->name('teknis.summary')->middleware('Admin');
         // Route::get('teknis/summary/{id}/detail', 'Dashboard\TeknisController@summary_detail')->name('teknis.summary.detail')->middleware('Admin');
         Route::get('teknis/summary/{id}/detail', 'Dashboard\TeknisController@summary_detail')->name('teknis.summary.detail');
